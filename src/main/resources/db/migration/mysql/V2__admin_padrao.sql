a319bd77dd06a0dfb921d81523d3e34cba7fd242
Yuri de Paula <yuri.oliveira@democrata.com.br>
2018-03-26 08:46:25 -0300

    migrations

--- src/main/resources/db/migration/mysql/V2__admin_padrao.sql
+++
@@ -1,7 +0,0 @@
-
-/* forcei manualmente no banco
-insert into usuario
-  (data_criacao, email, nome, senha)
-values
-(current_date(), 'yuriodp@gmail.com', 'yuri oliveira de paula', '$2a$10$nsbvP7oXSf2E.1YvEWmg4eyrRBqpL6T9TknwT7MZr.jmz250Pp/MO');
-*/
